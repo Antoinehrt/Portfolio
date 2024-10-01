6488b6e4de0b5a4f67a8b5fa0f33db24d9abca77
hauret <hauret.antoine@gmail.com>
2024-10-01 21:28:20 +0200

    feat(HomePage): import material library for icons

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
