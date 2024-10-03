c486e8430a68e1714f5ca2380e51b0335875f4f4
hauret <hauret.antoine@gmail.com>
2024-10-03 22:04:02 +0200

    feat(Architecture): updating the services architecture

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {Service} from "../../core/common/models/service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
-
+  services : Service[] = [
+    {name: 'Web Development', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n' +
+        '      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n' +
+        '      ex ea commodo consequat.'},
+    {name: 'Mobile Development', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n' +
+        '      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n' +
+        '      ex ea commodo consequat.'},
+    {name: 'Design', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n' +
+        '      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n' +
+        '      ex ea commodo consequat.'},
+    {name: 'Consulting', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\n' +
+        '      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\n' +
+        '      ex ea commodo consequat.'}
+  ];
 }
