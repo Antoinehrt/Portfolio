8d6630f488a0aabbeacc883f92e842edb0842614
hauret <hauret.antoine@gmail.com>
2024-10-07 19:21:09 +0200

    feat(ContactMe): create the contact me form

--- src/app/pages/contact-me/contact-me.component.ts
+++ src/app/pages/contact-me/contact-me.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.css'
 })
 export class ContactMeComponent {
+  contactForm: FormGroup;
 
+  constructor(private _fb: FormBuilder) {
+    this.contactForm = this._fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.minLength(10)]],
+      subject: ['', [Validators.required, Validators.minLength(5)]],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  sendMessage() {
+    console.log(this.contactForm.value);
+  }
 }
