7ad59e1939847230e186bf9b74ce3a354f76c875
Antoinehrt <hauret.antoine@gmail.com>
2024-10-08 00:37:33 +0200

    feat(ContactMe): send email when the form is complete and the "send message" button is clicked

--- src/app/pages/contact-me/contact-me.component.ts
+++ src/app/pages/contact-me/contact-me.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NgClass, NgIf} from "@angular/common";
+import emailjs from '@emailjs/browser';
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-contact-me',
@@ -28,5 +30,20 @@ export class ContactMeComponent {
 
   sendMessage() {
     console.log(this.contactForm.value);
+    emailjs.init(environment.EMAILJS_USER_ID)
+
+    emailjs.send(environment.EMAILJS_SERVICE_ID, environment.EMAILJS_TEMPLATE_ID, {
+      from_name: this.contactForm.value.name,
+      email: this.contactForm.value.email,
+      phone: this.contactForm.value.phone,
+      subject: this.contactForm.value.subject,
+      message: this.contactForm.value.message
+    })
+      // TODO: en faire un toast et clear le form en cas de success
+      .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+      }, function(error) {
+        console.log('FAILED...', error);
+    })
   }
 }
