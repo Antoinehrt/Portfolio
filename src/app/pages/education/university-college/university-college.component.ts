50bd8da9a745c24a3fc56ea80c1adc38b1cacfe9
Antoinehrt <hauret.antoine@gmail.com>
2024-11-08 16:22:48 +0100

    feat(EducationDetails): init university college component

---
+++ src/app/pages/education/university-college/university-college.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-university-college',
+  standalone: true,
+  imports: [],
+  templateUrl: './university-college.component.html',
+  styleUrl: './university-college.component.css'
+})
+export class UniversityCollegeComponent {
+
+}
