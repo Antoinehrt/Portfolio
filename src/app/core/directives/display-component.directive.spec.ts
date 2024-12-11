f8e46062deaec739c61f654f1ea4cc9bfd9cf88c
hauret <hauret.antoine@gmail.com>
2024-12-11 23:05:48 +0100

    feat(EducationDetails): remove unused directive

--- src/app/core/directives/display-component.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DisplayComponentDirective } from './display-component.directive';
-
-describe('DisplayComponentDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DisplayComponentDirective();
-    expect(directive).toBeTruthy();
-  });
-});
