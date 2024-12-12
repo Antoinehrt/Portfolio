af9f25848b97ffb6ba25a0d38c7c9101e4a6b9b8
hauret <hauret.antoine@gmail.com>
2024-12-12 15:06:35 +0100

    feat(EducationDetails): add the type to correspond to the json structure

--- src/app/core/models/experience-entry.ts
+++ src/app/core/models/experience-entry.ts
@@ -2,6 +2,7 @@
  * Represents an education timeline entry.
  */
 export interface ExperienceEntry {
+  type: string; // Type of the event (education or work)
   name: string;  // Name of the timeline entry
   date: Date;    // Date
   description: string; // Description
