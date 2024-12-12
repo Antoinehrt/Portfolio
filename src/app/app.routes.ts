b5125943c91db0fec4676b590fce38b26b22de97
hauret <hauret.antoine@gmail.com>
2024-12-12 15:05:41 +0100

    feat(EducationDetails): remove unused routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,10 @@
 import {Routes} from '@angular/router';
 import {EducationComponent} from './pages/education/education.component';
 import {HomeComponent} from "./pages/home/home.component";
-import {HighSchoolComponent} from "./pages/education/high-school/high-school.component";
-import {UniversityComponent} from "./pages/education/university/university.component";
+import {educationResolver} from "./core/resolvers/education.resolver";
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'education', component: EducationComponent},
-  {path: 'education/high-school', component: HighSchoolComponent},
-  {path: 'education/university', component: UniversityComponent},
   {path: '**', redirectTo: 'home'}
 ];
