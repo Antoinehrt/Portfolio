79cf1e43b77d31de8ef3207bf627c9287f33b502
hauret <hauret.antoine@gmail.com>
2024-12-12 15:09:00 +0100

    feat(EducationDetails): remove unused imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import {Routes} from '@angular/router';
-import {EducationComponent} from './pages/education/education.component';
 import {HomeComponent} from "./pages/home/home.component";
-import {educationResolver} from "./core/resolvers/education.resolver";
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
