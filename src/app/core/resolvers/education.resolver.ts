ceab2b1429df5e6a8b3710499d0bce5e76fc8ab3
hauret <hauret.antoine@gmail.com>
2024-12-12 15:10:04 +0100

    feat(EducationDetails): create a resolver to load the data and the page in the right order

--- src/app/core/resolvers/education.resolver.ts
+++ src/app/core/resolvers/education.resolver.ts
@@ -1,5 +1,16 @@
-import { ResolveFn } from '@angular/router';
+import {inject} from '@angular/core';
+import {StaticDataService} from '../services/static-data/static-data.service';
+import {ResolveFn} from '@angular/router';
+import {ExperienceEntry} from '../models/experience-entry';
+import {map} from 'rxjs';
 
-export const educationResolver: ResolveFn<boolean> = (route, state) => {
-  return true;
+export const educationResolver: ResolveFn<{ experienceEntry: ExperienceEntry[] }> = (route, state) => {
+  const staticDataService = inject(StaticDataService);
+  console.log('Resolver triggered');
+
+  return staticDataService.getStaticData().pipe(
+    map(data => {
+      console.log('Fetched static data in resolver:', data);
+      return {experienceEntry: data.experienceEntries};
+    }));
 };
