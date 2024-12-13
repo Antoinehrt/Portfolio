2570aecde2cf3eefed32780a9857189ea18ec2fc
hauret <hauret.antoine@gmail.com>
2024-12-13 11:59:50 +0100

    feat(EducationDetails): create the style of the details

--- src/app/pages/education/high-school/high-school.component.ts
+++ src/app/pages/education/high-school/high-school.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {EducationService} from "../../../core/services/education/education.service";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-high-school',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './high-school.component.html',
   styleUrl: './high-school.component.css'
 })
 export class HighSchoolComponent {
 
+  constructor(private _educationService: EducationService) {
+  }
+
+  navigateBack() {
+    this._educationService.currentComponent = "education"
+  }
 }
