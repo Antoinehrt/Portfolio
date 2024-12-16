ca4ca23b26e12f9ddf009b22c1226a05f4901fdb
hauret <hauret.antoine@gmail.com>
2024-12-16 19:09:44 +0100

    feat(EducationDetails): create content for university

--- src/app/pages/education/university/university.component.ts
+++ src/app/pages/education/university/university.component.ts
@@ -1,12 +1,21 @@
 import {Component} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {EducationService} from "../../../core/services/education/education.service";
 
 @Component({
   selector: 'app-university',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './university.component.html',
   styleUrl: './university.component.css'
 })
 export class UniversityComponent {
+  constructor(private _educationService: EducationService) {
+  }
 
+  navigateBack() {
+    this._educationService.currentComponent = "education"
+  }
 }
