4545029626d5ef90601edf701f80c78134c8b75d
hauret <hauret.antoine@gmail.com>
2024-12-17 13:39:02 +0100

    feat(EducationDetails): create the internship content

--- src/app/pages/education/internship/internship.component.ts
+++ src/app/pages/education/internship/internship.component.ts
@@ -1,12 +1,21 @@
 import {Component} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {EducationService} from "../../../core/services/education/education.service";
 
 @Component({
   selector: 'app-internship',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './internship.component.html',
   styleUrl: './internship.component.css'
 })
 export class InternshipComponent {
+  constructor(private _educationService: EducationService) {
+  }
 
+  navigateBack() {
+    this._educationService.currentComponent = "education"
+  }
 }
