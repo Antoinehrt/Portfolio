8cada1067850dc03bc79646ed722545f1c9f2556
AntoineHrt <hauret.antoine@gmail.com>
2025-11-25 22:02:16 +0100

    feat(DynamicProjects): refactor projects component to use project card component and implement data fetching

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,39 +1,49 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Project} from "../../core/models/project";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
+import {ProjectCardComponent} from "./project-card/project-card.component";
+import {ProjectsService} from "../../core/services/projects/projects.service";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf,
+    ProjectCardComponent
   ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent {
-  projects: Project[] = [
-    {
-      name: 'Project 1',
-      description: ' \'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\\n\' +\n' +
-        '        \'      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\\n\' +\n' +
-        '        \'      ex ea commodo consequat',
-      imageUrl: '../../../assets/img/square_profile_pic.jpg'
-    },
-    {
-      name: 'Project 2',
-      description: ' \'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\\n\' +\n' +
-        '        \'      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\\n\' +\n' +
-        '        \'      ex ea commodo consequat',
-      imageUrl: '../../../assets/img/square_profile_pic.jpg'
-    },
-    {
-      name: 'Project 3',
-      description: ' \'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut\\n\' +\n' +
-        '        \'      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip\\n\' +\n' +
-        '        \'      ex ea commodo consequat',
-      imageUrl: '../../../assets/img/square_profile_pic.jpg'
-    }
-  ];
+export class ProjectsComponent implements OnInit {
+  projects: Project[] = [];
+  isLoading = true;
+  error: string | null = null;
 
+  constructor(private projectsService: ProjectsService) {}
+
+  ngOnInit() {
+    this.loadProjects();
+  }
+
+  private loadProjects(): void {
+    this.isLoading = true;
+    this.error = null;
+    
+    this.projectsService.getPinnedRepositories().subscribe({
+      next: (projects) => {
+        this.projects = projects;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Error loading projects:', err);
+        this.error = 'Failed to load projects. Please try again later.';
+        this.isLoading = false;
+      }
+    });
+  }
+
+  retryLoading(): void {
+    this.loadProjects();
+  }
 }
