c8e9f71d6a6941ff1723d604f6463a299119ddfe
AntoineHrt <hauret.antoine@gmail.com>
2025-11-25 20:12:31 +0100

    feat(DynamicProjects): add FormatSizePipe for size formatting in KB, Mo, and Go

---
+++ src/app/core/pipes/size/format-size.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatSize',
+  standalone: true
+})
+export class FormatSizePipe implements PipeTransform {
+
+  transform(sizeInKB: number): string {
+    if (sizeInKB < 1024) {
+      return `${sizeInKB} Ko`;
+    } else if (sizeInKB < 1024 * 1024) {
+      return `${(sizeInKB / 1024).toFixed(2)} Mo`;
+    } else {
+      return `${(sizeInKB / (1024 * 1024)).toFixed(2)} Go`;
+    }
+  }
+
+}
